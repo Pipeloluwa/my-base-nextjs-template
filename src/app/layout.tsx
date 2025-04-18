fdfd72cbcaeb3ec15d82b8d47d980471aadc58f7
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2025-04-18 20:58:28 +0100

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { interFont } from "./fonts/fontsConfig";
 import "./globals.css";
+import StateProvider from "./state_management/provider";
+import PageContainer from "./Containers/PageContainer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interFont.className} antialiased`}
       >
-        {children}
+        <StateProvider>
+            <PageContainer>
+              {children}
+            </PageContainer>
+        </StateProvider>
       </body>
     </html>
   );
